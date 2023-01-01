0953c5cf2f5b0b7af8e730884d8536b18f1ae4e1
Cristhian Maciel <maciel.cristhian@gmail.com>
2023-01-01 13:30:45 -0300

    Count total prime numbers in an array

--- Swift Algorithms.playground/Pages/Algorithms.xcplaygroundpage/Contents.swift
+++ Swift Algorithms.playground/Pages/Algorithms.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+
 /*
  Determine if a positive integer is prime or not
  */
@@ -30,3 +31,30 @@ isPrime(num: 6)
 isPrime(num: 97)
 isPrime(num: 53)
 isPrime(num: 17)
+
+
+/*
+ Count total prime numbers of an array
+ */
+func countPrimes(arr: [Int]) -> Int{
+    var total: Int = 0
+    for i in 0..<arr.count {
+        if isPrime(num: arr[i]){
+            total += 1
+        }
+    }
+    return total
+}
+
+
+// Test cases
+var array1 : [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+countPrimes(arr: array1)
+array1 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+countPrimes(arr: array1)
+array1 = [74, 75, 76, 77, 78]
+countPrimes(arr: array1)
+
+
+
+
