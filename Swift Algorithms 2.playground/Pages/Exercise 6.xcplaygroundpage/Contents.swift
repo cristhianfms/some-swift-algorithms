a080bbf9a857ef7c1e92198d209038d01698f149
Cristhian Maciel <maciel.cristhian@mercadolibre.com>
2023-05-10 19:51:49 -0300

    exercices 2.6 and 2.7

--- Swift Algorithms 2.playground/Pages/Exercise 6.xcplaygroundpage/Contents.swift
+++ Swift Algorithms 2.playground/Pages/Exercise 6.xcplaygroundpage/Contents.swift
@@ -4,3 +4,22 @@ import Foundation
  Exercise 6:
  Given an array of integers, return a dictionary in which each unique number in the array is a key and the value is the number of times it appears consecutively within the array sent.
  */
+
+
+func numberOfOccurrences ( numbers: [Int]) -> [Int:Int] {
+    var ocurrences: [Int:Int] = [:]
+    for n in numbers {
+        if let value = ocurrences[n] {
+            ocurrences[n] = value + 1
+        } else {
+            ocurrences[n] = 1
+        }
+    }
+    
+    return ocurrences
+}
+
+
+let arrray1: [Int] = [1, 2, 3, 1, 2, 3, 4]
+
+numberOfOccurrences(numbers: arrray1)
--- Swift Algorithms 2.playground/Pages/Exercise 7.xcplaygroundpage/Contents.swift
+++ Swift Algorithms 2.playground/Pages/Exercise 7.xcplaygroundpage/Contents.swift
@@ -4,3 +4,22 @@ import Foundation
  Exercise 7:
  Given a sentence to a function, break it down into its individual words. Once done, return a dictionary with the words as keys and in the value, the number of times each word appears in the sentence.
  */
+
+func numberOfWordsOcurrences(sentence: String) -> [String: Int] {
+    var ocurrences: [String: Int] = [:]
+    let words = sentence.components(separatedBy: " ")
+    
+    for w in words {
+        if let value = ocurrences[w] {
+            ocurrences[w] = value + 1
+        } else {
+            ocurrences[w] = 1
+        }
+    }
+    
+    return ocurrences
+}
+
+
+let sentence1 = "Hi this is a test yes it's only a crazy test"
+numberOfWordsOcurrences(sentence: sentence1)
