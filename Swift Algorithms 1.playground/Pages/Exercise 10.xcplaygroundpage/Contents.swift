4b49f927bfd0d529026a2f57c9f9c03d7ecc5943
Cristhian Maciel <maciel.cristhian@mercadolibre.com>
2023-05-11 00:21:19 -0300

    exercise 1.10

--- Swift Algorithms 1.playground/Pages/Exercise 10.xcplaygroundpage/Contents.swift
+++ Swift Algorithms 1.playground/Pages/Exercise 10.xcplaygroundpage/Contents.swift
@@ -4,3 +4,20 @@ import Foundation
  Exercise 10:
  Write a program that stores various types of food and their price per kilogram in a dictionary. Create a way to show the price that any of the products would have when a specific number of kilograms is indicated.
  */
+
+
+var fruitPrices: [String: Float] = [
+    "Apple" : 12.5,
+    "Banana": 5.90,
+    "Carrot": 1.25,
+    "Pumpkin": 6.59
+]
+
+func getFruitPrice(fruit: String, weight: Float) -> Float? {
+    guard let pricePerKg = fruitPrices[fruit] else {return nil}
+            
+    return pricePerKg * weight
+}
+
+
+getFruitPrice(fruit: "Banana", weight: 2)
